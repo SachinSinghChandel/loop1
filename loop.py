86f5ca62e180c3aad6e0e72e0e9ce0bb633c7e53
SachinSinghChandel <sachinsinghbhp@gmail.com>
2025-09-26 21:18:22 +0530

    add commit

---
+++ loop.py
@@ -0,0 +1,19 @@
+count=1# count is iterator
+while count <= 10:
+    print("sachin ")
+    count = count+1 # count +=1
+print("loop is end  because count =11")
+
+i=1
+while i<=10:
+    print(2*i)
+    i +=1
+
+i=1
+while i <= 100:
+    print(i)
+    i+=1
+
+j=100
+while j >= 1:
+    print(j)
